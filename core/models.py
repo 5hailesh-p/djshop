90f1a90b1586f2462eb0212080bbc6c462accb20
5hailesh-p <shailesh.p2701@gmail.com>
2026-02-09 20:47:53 +0530

    product quantity

--- core/models.py
+++ core/models.py
@@ -35,6 +35,7 @@ class product(models.Model):
     product_cat = models.CharField(max_length=100)
     price = models.IntegerField()
     product_img  = models.ImageField(upload_to='product/img/')
+    product_quantity = models.IntegerField(default=0, null=True)
 
     def __str__(self):
         return self.product_name
