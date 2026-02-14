5b065d5ad361f4d5f3d433356dbd3f0c2e658893
5hailesh-p <shailesh.p2701@gmail.com>
2026-02-14 21:28:17 +0530

    home url style

--- core/views.py
+++ core/views.py
@@ -3,12 +3,15 @@ from .models import product
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
+# @login_required
 def home(request): 
+    fname = None
+    if request.user.is_authenticated:
+        fname =  request.user.first_name
     context = {
         # 'products' : product.objects.filter(product_cat__in=['vehicel','accessories']),
-        # 'products' : product.objects.all(),
-        'fname' : request.user.first_name,
+        'products' : product.objects.all(),
+        'fname' : fname,
     }
 
     return render(request, 'index.html', context)
