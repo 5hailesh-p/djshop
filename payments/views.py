9f422d4bcbdb1e81ef7c8df76ae7fd00d10b2d95
5hailesh-p <shailesh.p2701@gmail.com>
2026-03-10 14:28:11 +0530

    upated

--- payments/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
-
-# Create your views here.
-def payment(request): 
-    return HttpResponse('this is payment page')
